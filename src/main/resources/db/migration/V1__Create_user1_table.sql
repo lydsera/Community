8b019be08473e5aa3264d05fe6534496b04c0b70
lydsera <1048554745@qq.com>
2023-01-07 12:57:08 +0800

    add flyway migration support but error

---
+++ src/main/resources/db/migration/V1__Create_user1_table.sql
@@ -0,0 +1,12 @@
+
+create table PUBLIC.USER1
+(
+    ID           INTEGER auto_increment
+        primary key,
+    ACCOUNT_ID   CHARACTER VARYING(100),
+    NAME         CHARACTER VARYING(50),
+    TOKEN        CHARACTER(36),
+    GMT_CREATE   BIGINT,
+    GMT_MODIFIED BIGINT
+);
+
