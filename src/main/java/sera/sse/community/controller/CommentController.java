fc2379beea2e6d5b1abbb86746a5bea2f6958eb1
lydsera <1048554745@qq.com>
2023-01-10 19:07:43 +0800

    初步实现回复功能

---
+++ src/main/java/sera/sse/community/controller/CommentController.java
@@ -0,0 +1,31 @@
+package sera.sse.community.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import sera.sse.community.dto.CommentDTO;
+import sera.sse.community.mapper.CommentMapper;
+import sera.sse.community.model.Comment;
+
+@Controller
+public class CommentController {
+    @Autowired
+    private CommentMapper commentMapper;
+    @ResponseBody
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO){
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setCommentator(1);
+        comment.setLikeCount(0L);
+        commentMapper.insertSelective(comment);
+        return null;
+    }
+}
