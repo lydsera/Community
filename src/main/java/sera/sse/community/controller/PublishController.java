e2c7e1b51c065fd822ebee72850de52ffd58a889
lydsera <1048554745@qq.com>
2023-01-12 14:32:29 +0800

    创建标签库

--- src/main/java/sera/sse/community/controller/PublishController.java
+++ src/main/java/sera/sse/community/controller/PublishController.java
@@ -1,6 +1,7 @@
 package sera.sse.community.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sera.sse.community.cache.TagCache;
 import sera.sse.community.dto.InvitationDTO;
 import sera.sse.community.mapper.InvitationMapper;
 import sera.sse.community.model.Invitation;
@@ -27,10 +29,13 @@ public class PublishController {
         model.addAttribute("description",invitation.getDescription());
         model.addAttribute("tag",invitation.getTag());
         model.addAttribute("id",invitation.getId());
+
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
     @GetMapping("/publish")
-    public String publish(){
+    public String publish(Model model){
+        model.addAttribute("tags", TagCache.get());
         return  "publish";
     }
     @PostMapping("/publish")
@@ -45,6 +50,7 @@ public class PublishController {
         model.addAttribute("title",title);
         model.addAttribute("description",description);
         model.addAttribute("tag",tag);
+        model.addAttribute("tags", TagCache.get());
 
         if(title==null||title==""){
             model.addAttribute("error","标题不能为空！");
@@ -59,7 +65,11 @@ public class PublishController {
             return "publish";
         }
 
-
+        String invalid = TagCache.filterInvalid(tag);
+        if (StringUtils.isNotBlank(invalid)) {
+            model.addAttribute("error", "输入非法标签:" + invalid);
+            return "publish";
+        }
 
         User user = (User) request.getSession().getAttribute("user");
 
